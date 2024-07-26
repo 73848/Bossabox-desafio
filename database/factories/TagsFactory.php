8b10527e8c2c7fdcf1c5bf68ce7efbe0164bda4a
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-07-26 10:31:44 -0300

    configuracoes das factorys

---
+++ database/factories/TagsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tags>
+ */
+class TagsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+                'id'=>fake()->unique()->numerify(),
+                'name'=>fake()->randomElements(['PHP', 'Node', 'Java', 'Laravel', 'PHP Faker', 'Composer', 'json', 'http2', 'api', 'web', 'rest']),
+        ];
+    }
+}
