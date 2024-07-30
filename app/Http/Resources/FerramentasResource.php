1be91de4a0f48d774de67841f16af619357f8bf7
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-07-30 11:47:48 -0300

    Configurando TagResource para usar dentro da FerramentasResource, padronizando a resposta do relacionamento entre os modelos

--- app/Http/Resources/FerramentasResource.php
+++ app/Http/Resources/FerramentasResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-
+use App\Http\Resources\TagsResource;
 class FerramentasResource extends JsonResource
 {
     /**
@@ -18,7 +18,8 @@ class FerramentasResource extends JsonResource
             'id'=> $this->id,
             'title'=> $this->title,
             'link'=> $this->link,
-            'description'=> $this->description
+            'description'=> $this->description,
+            'tags'=> TagsResource::collection($this->tags)
         ];
     }
 }
